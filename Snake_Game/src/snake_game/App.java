e6b5cdb0d3299ca8405114007b0eaf8a835bbf7e
Akash Kumar <akashkr270605@gmail.com>
2024-12-30 00:33:13 +0530

    Add files via upload

---
+++ Snake_Game/src/snake_game/App.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snake_game;
+
+import javax.swing.*;
+/**
+ *
+ * @author darknight
+ */
+public class App {
+
+    
+    public static void main(String[] args) {
+       int boardWidth= 600;
+       int boardHeight= boardWidth;
+       
+       JFrame frame= new JFrame("Snake");
+       frame.setVisible(true);
+       frame.setSize(boardWidth, boardHeight);
+       frame.setLocationRelativeTo(null);
+       frame.setResizable(false);
+       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       
+       SnakeGame snakeGame= new SnakeGame(boardWidth, boardHeight);
+       frame.add(snakeGame);
+       frame.pack();
+       snakeGame.requestFocus();
+    }
+    
+}
